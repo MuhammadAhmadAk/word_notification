03f9591c6e46e3e9143b960f2aa1b13f9f55c3ea
Muhammad Ahmad Akhtar <muhammadahmad9304@gmai.com>
2025-04-22 10:04:36 +0500

    port update

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use("/api/notifications", depositRoutes);
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT ?? 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
